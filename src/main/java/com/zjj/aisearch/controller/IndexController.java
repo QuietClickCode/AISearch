80cc60a48be3cb1e11142010231919628160135a
zjjlovetj <2572417548@qq.com>
2019-09-29 13:01:16 +0800

    爬虫分支

--- src/main/java/com/zjj/aisearch/controller/IndexController.java
+++ src/main/java/com/zjj/aisearch/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.zjj.aisearch.model.Article;
 import com.zjj.aisearch.model.SearchRecord;
 import com.zjj.aisearch.service.IndexService;
 import com.zjj.aisearch.utils.DateConvert;
+import com.zjj.aisearch.utils.HTMLUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,7 +37,7 @@ public class IndexController implements PageProcessor {
     @ResponseBody
     public void search(String keyword) {
         System.out.println(indexService + "----------------");
-        Spider.create(this).addUrl("https://blog.csdn.net/").run();
+        Spider.create(this).addUrl("https://www.jianshu.com").thread(1).run();
         SearchRecord searchRecord = new SearchRecord();
         searchRecord.setKeyword(keyword);
         searchRecord.setSearchTime(DateConvert.getTime());
@@ -56,10 +57,18 @@ public class IndexController implements PageProcessor {
 
     @Override
     public void process(Page page) {
-        page.addTargetRequests(page.getHtml().links().regex("https://blog.csdn.net/[a-z 0-9 -]+/article/details/[0-9]{8}").all());
-        page.putField("title", page.getHtml().xpath("//*[@id=\"mainBox\"]/main/div[1]/div/div/div[1]/h1"));
-        page.putField("content", page.getHtml().xpath("//*[@id=\"mainBox\"]/main/div[1]"));
-
+        /*page.addTargetRequests(page.getHtml().links().regex("https://blog.csdn.net/[a-z 0-9 -]+/article/details/[0-9]{9}").all());*/
+        page.addTargetRequests(page.getHtml().links().regex("https://www.jianshu.com/p/[a-z 0-9 -]+").all());
+        /*dengyao-blogs/p/11606929.html*/
+        /*page.addTargetRequests(page.getHtml().links().regex("https://blog.csdn.net/leixiaohua1020/article/details/18893769").all());*/
+        System.out.println(page.toString() + "*********************************************");
+        System.out.println(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1") + "--------------------------------" + page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article"));
+        System.out.println(!(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1").equals("")));
+        System.out.println(!(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1")==null) && !(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article")==null));
+        if (!(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1")==null) && !(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article")==null)) {
+            page.putField("title", HTMLUtil.delHTMLTag(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1").toString()));
+            page.putField("content", HTMLUtil.delHTMLTag(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article").toString()));
+        }
         Article article = new Article();
         article.setContent(page.getResultItems().get("content") == null ? "" : page.getResultItems().get("content").toString());
         article.setTitle(page.getResultItems().get("title") == null ? "" : page.getResultItems().get("title").toString());
@@ -73,6 +82,6 @@ public class IndexController implements PageProcessor {
 
     @Override
     public Site getSite() {
-        return Site.me().setSleepTime(100).setRetryTimes(3);
+        return Site.me().setSleepTime(1000).setRetryTimes(1);
     }
 }
