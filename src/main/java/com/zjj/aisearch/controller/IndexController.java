556e59e894d56f333dba44bfff0cbc868d8e2215
zjjlovetj <2572417548@qq.com>
2019-09-29 15:07:09 +0800

    爬虫分支

--- src/main/java/com/zjj/aisearch/controller/IndexController.java
+++ src/main/java/com/zjj/aisearch/controller/IndexController.java
@@ -36,52 +36,34 @@ public class IndexController implements PageProcessor {
     @RequestMapping("search")
     @ResponseBody
     public void search(String keyword) {
-        System.out.println(indexService + "----------------");
-        Spider.create(this).addUrl("https://www.jianshu.com").thread(1).run();
+        Spider.create(this).addUrl("https://www.jianshu.com").thread(3).run();
         SearchRecord searchRecord = new SearchRecord();
         searchRecord.setKeyword(keyword);
         searchRecord.setSearchTime(DateConvert.getTime());
-
         int i = indexService.insertSearchRecord(searchRecord);
-        System.out.println(i);
-
-        Article article = new Article();
-        article.setCreateTime(DateConvert.getTime());
-        article.setTitle("dd");
-        article.setContent("fjak");
-
-        indexService.insertArticle(article);
-
     }
 
 
     @Override
     public void process(Page page) {
-        /*page.addTargetRequests(page.getHtml().links().regex("https://blog.csdn.net/[a-z 0-9 -]+/article/details/[0-9]{9}").all());*/
-        page.addTargetRequests(page.getHtml().links().regex("https://www.jianshu.com/p/[a-z 0-9 -]+").all());
-        /*dengyao-blogs/p/11606929.html*/
-        /*page.addTargetRequests(page.getHtml().links().regex("https://blog.csdn.net/leixiaohua1020/article/details/18893769").all());*/
-        System.out.println(page.toString() + "*********************************************");
-        System.out.println(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1") + "--------------------------------" + page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article"));
-        System.out.println(!(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1").equals("")));
-        System.out.println(!(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1")==null) && !(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article")==null));
-        if (!(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1")==null) && !(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article")==null)) {
-            page.putField("title", HTMLUtil.delHTMLTag(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1").toString()));
-            page.putField("content", HTMLUtil.delHTMLTag(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article").toString()));
+        page.addTargetRequests(page.getHtml().links().regex("https://www.jianshu.com/p/[a-z 0-9]{12}").all());
+        String title = page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1").get();
+        String content = page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article").get();
+        if (title != null && content != null && title != "" && content != "") {
+            page.putField("title", HTMLUtil.delHTMLTag(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div/section[1]/h1").get().toString()));
+            page.putField("content", HTMLUtil.delHTMLTag(page.getHtml().xpath("//*[@id=\"__next\"]/div[1]/div/div[1]/section[1]/article").get().toString()));
+            Article article = new Article();
+            article.setContent(page.getResultItems().get("content") == null ? "" : page.getResultItems().get("content").toString());
+            article.setTitle(page.getResultItems().get("title") == null ? "" : page.getResultItems().get("title").toString());
+            article.setCreateTime(DateConvert.getTime());
+            this.indexService.insertArticle(article);
+            System.out.println(article.getId());
         }
-        Article article = new Article();
-        article.setContent(page.getResultItems().get("content") == null ? "" : page.getResultItems().get("content").toString());
-        article.setTitle(page.getResultItems().get("title") == null ? "" : page.getResultItems().get("title").toString());
-        article.setCreateTime(DateConvert.getTime());
-        System.out.println(article.toString());
 
-        System.out.println(this.indexService + "===========================");
-        this.indexService.insertArticle(article);
-        page.putField("article", article);
     }
 
     @Override
     public Site getSite() {
-        return Site.me().setSleepTime(1000).setRetryTimes(1);
+        return Site.me().setSleepTime(300).setRetryTimes(3);
     }
 }
