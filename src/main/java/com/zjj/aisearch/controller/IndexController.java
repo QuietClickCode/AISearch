eb1c9428ad31c36a9833c2989ac55c0e9e6c59ec
zjjlovetj <2572417548@qq.com>
2019-10-01 23:32:06 +0800

    controller完善了注释,搞成文档注释,更好看了

--- src/main/java/com/zjj/aisearch/controller/IndexController.java
+++ src/main/java/com/zjj/aisearch/controller/IndexController.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
 
-/**
+/***
  * @program: AISearch
  * @description:
  * @author: zjj
@@ -30,16 +30,23 @@ public class IndexController {
     @Autowired
     private IndexService indexServiceImpl;
 
-    /*这样用可能有缺陷,内存泄漏*/
+    /**
+     * 这样用可能有缺陷,内存泄漏
+     */
     private HashMap<String, Object> map = new HashMap<>();
 
-    /*实现转发模式,/index */
+    /**
+     * 实现转发模式,/index
+     */
     @RequestMapping(value = "{path}")
     public String del(@PathVariable("path") String path) {
         return path;
     }
 
 
+    /**
+     * 登录
+     */
     @RequestMapping("/tologin")
     @ResponseBody
     public String tologin(@RequestBody UserInfo userInfo, HttpServletRequest request) {
@@ -69,7 +76,7 @@ public class IndexController {
                 loginLog.setBrowserInfoId(browserInfoId);
                 loginLog.setLocationId(locationId);
                 loginLog.setUserId(userId);
-                /*写入登录日志*/
+                /**写入登录日志*/
                 indexServiceImpl.insertLoginLog(loginLog);
                 request.getSession().setAttribute("user", isExistUser);
 
@@ -82,6 +89,9 @@ public class IndexController {
         }
     }
 
+    /**
+     * 注册
+     */
     @RequestMapping("/toregist")
     @ResponseBody
     public String toregist(@RequestBody UserInfo userInfo, HttpServletRequest request) {
@@ -111,7 +121,9 @@ public class IndexController {
         return null;
     }
 
-
+    /**
+     * 进入首页
+     */
     @RequestMapping("/index")
     public String index(HttpServletRequest request, Model model, RedirectAttributes redirectAttributes) {
         User user = (User) request.getSession().getAttribute("user");
@@ -125,12 +137,18 @@ public class IndexController {
 
     }
 
+    /**
+     * 进入首页
+     */
     @RequestMapping("/")
     public String index() {
         return "redirect:index";
 
     }
 
+    /**
+     * ajax实时搜索
+     */
     @RequestMapping("/searchItem")
     @ResponseBody
     public Object searchItem(String keyword, HttpServletResponse res) throws IOException {
@@ -141,7 +159,9 @@ public class IndexController {
         return null;
     }
 
-    /*进入命令模式*/
+    /**
+     * 进入命令模式
+     */
     @RequestMapping("/command")
     @ResponseBody
     public String command(@RequestBody Info info, HttpServletRequest request) throws IOException {
@@ -178,7 +198,82 @@ public class IndexController {
         return "其他操作";
     }
 
-    /*便签模式*/
+    /**
+     * 重定向到搜索结果详情页
+     */
+    @RequestMapping("/todetail")
+    public String toDetail(@RequestBody Info info, RedirectAttributes attributes, HttpServletRequest httpServletRequest, HttpServletResponse res) throws IOException {
+        String id = httpServletRequest.getSession().getId();
+
+        if (info.getKeyword().equals(":article")) {
+            return "redirect:article";
+        }
+        if (!info.getKeyword().isEmpty()) {
+            /**125.84.181.44,重庆市重庆市,29.56471,106.55073
+             windows,chrome,74.0.3729.131*/
+            String[] locationArr = info.getLocation();
+            String[] browserInfoArr = info.getBrowserInfo();
+            String pcOrPhone = info.getPcOrPhone();
+            BrowserInfo bi = new BrowserInfo();
+            Location lo = new Location();
+            bi.setSystem(browserInfoArr[0]);
+            bi.setBrowserType(browserInfoArr[1]);
+            bi.setBrowserVersion(browserInfoArr[2]);
+            lo.setIp(locationArr[0]);
+            lo.setLocation(locationArr[1]);
+            lo.setX(locationArr[2]);
+            lo.setY(locationArr[3]);
+            lo.setKeyword(pcOrPhone);
+            lo.setLocalIp(info.getLocalIp());
+            indexServiceImpl.insertBrowserInfo(bi);
+            indexServiceImpl.insertLocation(lo);
+
+
+            SearchRecord searchRecord = new SearchRecord();
+            searchRecord.setBrowserInfoId(bi.getBrowserInfoId());
+            searchRecord.setLocationId(lo.getLocationId());
+            searchRecord.setSearchTime(new Date().toLocaleString());
+            searchRecord.setKeyword(info.getKeyword());
+            indexServiceImpl.insertSearchRecord(searchRecord);
+            List<Item> items = indexServiceImpl.searchItem(info.getKeyword());
+            attributes.addFlashAttribute(id + "-" + "items1", items);
+            /**这里不是很懂,不重定向会报错,重定向,前端页面不主动+window.location跳转也不生效,这儿必须前后端配合,缺一不可*/
+            return "redirect:detail";
+        }
+        return null;
+
+    }
+
+    /**
+     * 进入搜索结果详情页
+     */
+    @RequestMapping("/detail")
+    public ModelAndView detail(HttpServletRequest request, ModelAndView modelAndView, HttpServletRequest
+            httpServletRequest, HttpServletResponse res) throws IOException {
+        String id = httpServletRequest.getSession().getId();
+        Map<String, ?> maps = RequestContextUtils.getInputFlashMap(request);
+        List<Item> list = null;
+        if (maps != null) {
+            list = (List<Item>) maps.get(id + "-" + "items1");
+        }
+
+        modelAndView.setViewName("detail");
+
+        if (list != null) {
+            map.put(id + "-" + "items1", list);
+            modelAndView.addObject("items", list);
+            return modelAndView;
+        } else {
+            List<Item> lists = (List<Item>) map.get(id + "-" + "items1");
+            modelAndView.addObject("items", lists);
+            return modelAndView;
+
+        }
+    }
+
+    /**
+     * 便签模式
+     */
     @RequestMapping("/note")
     @ResponseBody
     public String note(@RequestBody Info info) {
@@ -208,76 +303,9 @@ public class IndexController {
         return info.getKeyword();
     }
 
-
-    @RequestMapping("/todetail")
-    public String toDetail(@RequestBody Info info, RedirectAttributes attributes, HttpServletRequest httpServletRequest, HttpServletResponse res) throws IOException {
-        String id = httpServletRequest.getSession().getId();
-
-        if (info.getKeyword().equals(":article")) {
-            return "redirect:article";
-        }
-        if (!info.getKeyword().isEmpty()) {
-            /*125.84.181.44,重庆市重庆市,29.56471,106.55073
-                    windows,chrome,74.0.3729.131*/
-            String[] locationArr = info.getLocation();
-            String[] browserInfoArr = info.getBrowserInfo();
-            String pcOrPhone = info.getPcOrPhone();
-            BrowserInfo bi = new BrowserInfo();
-            Location lo = new Location();
-            bi.setSystem(browserInfoArr[0]);
-            bi.setBrowserType(browserInfoArr[1]);
-            bi.setBrowserVersion(browserInfoArr[2]);
-            lo.setIp(locationArr[0]);
-            lo.setLocation(locationArr[1]);
-            lo.setX(locationArr[2]);
-            lo.setY(locationArr[3]);
-            lo.setKeyword(pcOrPhone);
-            lo.setLocalIp(info.getLocalIp());
-            indexServiceImpl.insertBrowserInfo(bi);
-            indexServiceImpl.insertLocation(lo);
-
-
-            SearchRecord searchRecord = new SearchRecord();
-            searchRecord.setBrowserInfoId(bi.getBrowserInfoId());
-            searchRecord.setLocationId(lo.getLocationId());
-            searchRecord.setSearchTime(new Date().toLocaleString());
-            searchRecord.setKeyword(info.getKeyword());
-            indexServiceImpl.insertSearchRecord(searchRecord);
-            List<Item> items = indexServiceImpl.searchItem(info.getKeyword());
-            attributes.addFlashAttribute(id + "-" + "items1", items);
-            /*这里不是很懂,不重定向会报错,重定向,前端页面不主动+window.location跳转也不生效,这儿必须前后端配合,缺一不可*/
-            return "redirect:detail";
-        }
-        return null;
-
-    }
-
-    @RequestMapping("/todetail2")
-    public String toDetail2(String keyword, RedirectAttributes attributes) {
-        if (!keyword.isEmpty()) {
-            SearchRecord searchRecord = new SearchRecord();
-            searchRecord.setSearchTime(new Date().toLocaleString());
-            searchRecord.setKeyword(keyword);
-            indexServiceImpl.insertSearchRecord(searchRecord);
-            List<Item> items = indexServiceImpl.searchItem(keyword);
-
-            attributes.addFlashAttribute("items2", items);
-            return "redirect:detail2";
-        }
-        return null;
-    }
-
-
-    @RequestMapping("/todetail3")
-    public String toDetai3(String keyword, RedirectAttributes attributes) {
-        if (!keyword.isEmpty()) {
-            map.put("keyword", keyword);
-            return "redirect:detail3";
-        }
-        return null;
-    }
-
-    /*随机csdn文章功能*/
+    /**
+     * 随机csdn文章功能
+     */
     @RequestMapping("/article")
     public ModelAndView article(HttpServletRequest request, ModelAndView modelAndView, HttpServletRequest
             httpServletRequest, HttpServletResponse res) throws IOException {
@@ -295,30 +323,80 @@ public class IndexController {
         return modelAndView;
     }
 
-    @RequestMapping("/detail")
-    public ModelAndView detail(HttpServletRequest request, ModelAndView modelAndView, HttpServletRequest
-            httpServletRequest, HttpServletResponse res) throws IOException {
-        String id = httpServletRequest.getSession().getId();
-        Map<String, ?> maps = RequestContextUtils.getInputFlashMap(request);
-        List<Item> list = null;
-        if (maps != null) {
-            list = (List<Item>) maps.get(id + "-" + "items1");
-        }
-
-        modelAndView.setViewName("detail");
-
-        if (list != null) {
-            map.put(id + "-" + "items1", list);
-            modelAndView.addObject("items", list);
-            return modelAndView;
-        } else {
-            List<Item> lists = (List<Item>) map.get(id + "-" + "items1");
-            modelAndView.addObject("items", lists);
-            return modelAndView;
-
-        }
+    /**
+     * 搜索记录详情列表
+     */
+    @RequestMapping("/list")
+    public String list(Model model) {
+        List<SearchRecordLocation> searchRecordLocation = indexServiceImpl.selectSearchRecordLocation();
+        model.addAttribute("items", searchRecordLocation);
+        return "list";
     }
 
+    /**
+     * 便签记录详情列表
+     */
+    @RequestMapping("/ainote")
+    public String aiNotelist(Model model) {
+        List<AiNoteLocation> aiNoteLocation = indexServiceImpl.selectAiNoteLocation();
+        model.addAttribute("items", aiNoteLocation);
+        return "aiNoteList";
+    }
+
+    /**
+     * 注册用户列表
+     */
+    @RequestMapping("/userlist")
+    public String userList(Model model) {
+        List<UserLocation> userLocations = indexServiceImpl.selectUserLocation();
+        model.addAttribute("items", userLocations);
+        return "userlist";
+    }
+
+    /**
+     * 登录日志列表
+     */
+    @RequestMapping("/loginloglist")
+    public String loginLogList(Model model) {
+        List<LoginLogLocation> loginLogLocation = indexServiceImpl.selectLoginLocation();
+        model.addAttribute("items", loginLogLocation);
+        return "loginloglist";
+    }
+
+    /**
+     * 退出日志列表
+     */
+    @RequestMapping("/logoutloglist")
+    public String logoutLogList(Model model) {
+        List<LogoutLogLocation> logoutLogLocation = indexServiceImpl.selectLogoutLocation();
+        model.addAttribute("items", logoutLogLocation);
+        return "logoutloglist";
+    }
+
+
+    @RequestMapping("/todetail2")
+    public String toDetail2(String keyword, RedirectAttributes attributes) {
+        if (!keyword.isEmpty()) {
+            SearchRecord searchRecord = new SearchRecord();
+            searchRecord.setSearchTime(new Date().toLocaleString());
+            searchRecord.setKeyword(keyword);
+            indexServiceImpl.insertSearchRecord(searchRecord);
+            List<Item> items = indexServiceImpl.searchItem(keyword);
+
+            attributes.addFlashAttribute("items2", items);
+            return "redirect:detail2";
+        }
+        return null;
+    }
+
+    @RequestMapping("/todetail3")
+    public String toDetai3(String keyword, RedirectAttributes attributes) {
+        if (!keyword.isEmpty()) {
+            map.put("keyword", keyword);
+            return "redirect:detail3";
+        }
+        return null;
+    }
 
     @RequestMapping("/detail2")
     public ModelAndView detail2(HttpServletRequest request, ModelAndView modelAndView) {
@@ -351,45 +429,8 @@ public class IndexController {
             map.put("items3", items);
             return items;
         } else {
-
             return (List<Item>) map.get("items3");
         }
-    }
-
-
-    /*搜索记录详情列表*/
-    @RequestMapping("/list")
-    public String list(Model model) {
-        List<SearchRecordLocation> searchRecordLocation = indexServiceImpl.selectSearchRecordLocation();
-        model.addAttribute("items", searchRecordLocation);
-        return "list";
-    }
-
-    /*便签记录详情列表*/
-    @RequestMapping("/ainote")
-    public String aiNotelist(Model model) {
-        List<AiNoteLocation> aiNoteLocation = indexServiceImpl.selectAiNoteLocation();
-        model.addAttribute("items", aiNoteLocation);
-        return "aiNoteList";
-    }
-
-    @RequestMapping("/userlist")
-    public String userList(Model model) {
-        List<UserLocation> userLocations = indexServiceImpl.selectUserLocation();
-        model.addAttribute("items", userLocations);
-        return "userlist";
-    }
-    @RequestMapping("/loginloglist")
-    public String loginLogList(Model model) {
-        List<LoginLogLocation> loginLogLocation = indexServiceImpl.selectLoginLocation();
-        model.addAttribute("items", loginLogLocation);
-        return "loginloglist";
-    }
-    @RequestMapping("/logoutloglist")
-    public String logoutLogList(Model model) {
-        List<LogoutLogLocation> logoutLogLocation = indexServiceImpl.selectLogoutLocation();
-        model.addAttribute("items", logoutLogLocation);
-        return "logoutloglist";
     }
 
 }
