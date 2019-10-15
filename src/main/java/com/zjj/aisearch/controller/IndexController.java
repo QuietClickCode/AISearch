6c7485d70b89fcc6d6ff8c202fef3c1d31060edc
zjjlovetj <2572417548@qq.com>
2019-10-16 00:11:47 +0800

    终于解决了那个抓狂的问题,没有图标,因为css版本有点问题,估计,引用cdn都没得问题了,swagger-ui是因为自己哈一哈的做了个转发功能,导致swagger-ui跳转不了,现在就可以开始vue+spring boot纯前后端分离的模式开发了,还需要解决一个问题,数据通信用json+ajax,spring boot 做成restFul,前端怎么跳转??

--- src/main/java/com/zjj/aisearch/controller/IndexController.java
+++ src/main/java/com/zjj/aisearch/controller/IndexController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
