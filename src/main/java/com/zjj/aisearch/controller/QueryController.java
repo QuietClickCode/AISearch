c624b41ef3656e9e3b54840ccb69d0055e8d5c6f
zjjlovetj <2572417548@qq.com>
2019-10-22 15:24:57 +0800

    第一次用postman测试自己做的restful接口,通了

--- src/main/java/com/zjj/aisearch/controller/QueryController.java
+++ src/main/java/com/zjj/aisearch/controller/QueryController.java
@@ -7,8 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,126 +25,125 @@ public class QueryController {
     @Autowired
     private QueryService queryServiceImpl;
 
-    @RequestMapping("querySystemLog")
+    @PostMapping("querySystemLog")
     @ApiOperation("根据条件查询系统操作日志数据")
     public List<SystemLogList> querySystemLog(@RequestBody QueryForm queryForm) {
         return queryServiceImpl.queryForm(queryForm);
     }
 
-    @RequestMapping("queryCount")
+    @PostMapping("queryCount")
     @ApiOperation("根据条件查询系统操作日志数据总条数")
     public Integer queryCount(@RequestBody QueryForm queryForm) {
         Integer count = queryServiceImpl.queryCount(queryForm);
         return count;
     }
 
-    @RequestMapping("queryAiNote")
-    @ResponseBody
+    @PostMapping("queryAiNote")
     @ApiOperation("根据条件查询便签记录数据")
     public List<SystemLogList> queryAiNote(@RequestBody QueryForm queryForm) {
         return queryServiceImpl.queryAiNote(queryForm);
     }
 
-    @RequestMapping("queryAiNoteCount")
+    @PostMapping("queryAiNoteCount")
     @ApiOperation("根据条件查询便签记录数据总条数")
     public Integer queryAiNoteCount(@RequestBody QueryForm queryForm) {
         Integer count = queryServiceImpl.queryAiNoteCount(queryForm);
         return count;
     }
 
-    @RequestMapping("queryEditor")
+    @PostMapping("queryEditor")
     @ApiOperation("根据条件查询editor记录数据")
     public List<SystemLogList> queryEditor(@RequestBody QueryForm queryForm) {
         return queryServiceImpl.queryEditor(queryForm);
     }
 
-    @RequestMapping("queryEditorCount")
+    @PostMapping("queryEditorCount")
     @ApiOperation("根据条件查询editor记录数据总条数")
     public Integer queryEditorCount(@RequestBody QueryForm queryForm) {
         Integer count = queryServiceImpl.queryEditorCount(queryForm);
         return count;
     }
 
-    @RequestMapping("queryList")
+    @PostMapping("queryList")
     @ApiOperation("根据条件查询搜索记录数据")
     public List<SystemLogList> queryList(@RequestBody QueryForm queryForm) {
         return queryServiceImpl.queryList(queryForm);
     }
 
-    @RequestMapping("queryListCount")
+    @PostMapping("queryListCount")
     @ApiOperation("根据条件查询搜索记录数据总条数")
     public Integer queryListCount(@RequestBody QueryForm queryForm) {
         Integer count = queryServiceImpl.queryListCount(queryForm);
         return count;
     }
 
-    @RequestMapping("queryLoginLogList")
+    @PostMapping("queryLoginLogList")
     @ApiOperation("根据条件查询登录日志记录数据")
     public List<SystemLogList> queryLoginLogList(@RequestBody QueryForm queryForm) {
         return queryServiceImpl.queryLoginLogList(queryForm);
     }
 
-    @RequestMapping("queryLoginLogListCount")
+    @PostMapping("queryLoginLogListCount")
     @ApiOperation("根据条件查询登录日志记录数据总条数")
     public Integer queryLoginLogListCount(@RequestBody QueryForm queryForm) {
         Integer count = queryServiceImpl.queryLoginLogListCount(queryForm);
         return count;
     }
 
-    @RequestMapping("queryLogoutLogList")
+    @PostMapping("queryLogoutLogList")
     @ApiOperation("根据条件查询退出日志记录数据")
     public List<SystemLogList> queryLogoutLogList(@RequestBody QueryForm queryForm) {
         return queryServiceImpl.queryLogoutLogList(queryForm);
     }
 
-    @RequestMapping("queryLogoutLogListCount")
+    @PostMapping("queryLogoutLogListCount")
     @ApiOperation("根据条件查询退出日志记录数据总条数")
     public Integer queryLogoutLogListCount(@RequestBody QueryForm queryForm) {
         Integer count = queryServiceImpl.queryLogoutLogListCount(queryForm);
         return count;
     }
 
-    @RequestMapping("queryMarkdownList")
+    @PostMapping("queryMarkdownList")
     @ApiOperation("根据条件查询Markdown日志记录数据")
     public List<SystemLogList> queryMarkdownList(@RequestBody QueryForm queryForm) {
         return queryServiceImpl.queryMarkdownList(queryForm);
     }
 
-    @RequestMapping("queryMarkdownListCount")
+    @PostMapping("queryMarkdownListCount")
     @ApiOperation("根据条件查询Markdown日志记录数据总条数")
     public Integer queryMarkdownListCount(@RequestBody QueryForm queryForm) {
         Integer count = queryServiceImpl.queryMarkdownListCount(queryForm);
         return count;
     }
 
-    @RequestMapping("queryUserList")
+    @PostMapping("queryUserList")
     @ApiOperation("根据条件查询用户记录数据")
     public List<SystemLogList> queryUserList(@RequestBody QueryForm queryForm) {
         return queryServiceImpl.queryUserList(queryForm);
     }
 
-    @RequestMapping("queryUserListCount")
+    @PostMapping("queryUserListCount")
     @ApiOperation("根据条件查询用户记录数据总条数")
     public Integer queryUserListCount(@RequestBody QueryForm queryForm) {
         Integer count = queryServiceImpl.queryUserListCount(queryForm);
         return count;
     }
 
-    @RequestMapping("querySystem")
+    @PostMapping("querySystem")
     @ApiOperation("querySystem")
     public List<String> querySystem() {
         List<String> strings = queryServiceImpl.querySystem();
         return strings;
     }
 
-    @RequestMapping("queryDevice")
+    @PostMapping("queryDevice")
     @ApiOperation("queryDevice")
     public List<String> queryDevice() {
         List<String> strings = queryServiceImpl.queryDevice();
         return strings;
     }
 
-    @RequestMapping("queryBrowser")
+    @PostMapping("queryBrowser")
     @ApiOperation("queryBrowser")
     public List<String> queryBrowser() {
         List<String> strings = queryServiceImpl.queryBrowser();
