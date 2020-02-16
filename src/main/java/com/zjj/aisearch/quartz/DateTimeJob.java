fb62f4da755834a91e2066e8952343389818532e
QuietClickCode <2572417548@qq.com>
2020-02-16 22:32:56 +0800

    2020年2月16日22:32:23 https://blog.csdn.net/chenmingxu438521/article/details/94485695 spring boot整合quartz

---
+++ src/main/java/com/zjj/aisearch/quartz/DateTimeJob.java
@@ -0,0 +1,18 @@
+package com.zjj.aisearch.quartz;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTimeJob extends QuartzJobBean {
+ 
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        //获取JobDetail中关联的数据
+        String msg = (String) jobExecutionContext.getJobDetail().getJobDataMap().get("msg");
+        System.out.println("current time :"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "---" + msg);
+    }
+}
