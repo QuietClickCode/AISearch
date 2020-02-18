b8e3cb159f5e661ba0d2b62f5d7d7a1d5c54ec96
QuietClickCode <2572417548@qq.com>
2020-02-19 02:13:51 +0800

    考虑通过http方式控制爬虫程序暂停和启动和终止,考虑多线程

--- src/main/java/com/zjj/aisearch/crawler/Main.java
+++ src/main/java/com/zjj/aisearch/crawler/Main.java
@@ -28,26 +28,26 @@ public class Main {
         int start;//每页多少条
         int total = 0;//记录数
         int end = 9979;//总共9979条数据
-        for (start  = 0; start <= end; start += 20)  {
+        for (start = 0; start <= end; start += 20) {
             try {
 
                 String address = "https://Movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=&start=" + start;
 
                 JSONObject dayLine = new GetJson().getHttpJson(address, 1);
 
-                    System.out.println("start:" + start);
-                    JSONArray json = dayLine.getJSONArray("data");
-                    List<Movie> list = JSON.parseArray(json.toString(), Movie.class);
+                System.out.println("start:" + start);
+                JSONArray json = dayLine.getJSONArray("data");
+                List<Movie> list = JSON.parseArray(json.toString(), Movie.class);
 
-                    if (start <= end){
-                        System.out.println("已经爬取到底了");
-                    }
-                    for (Movie movie : list) {
-                        indexServiceImpl.insertMovie(movie);
-                    }
-                    total += list.size();
-                    System.out.println("正在爬取中---共抓取:" + total + "条数据");
-
+                System.exit(0);
+                if (start <= end) {
+                    System.out.println("已经爬取到底了");
+                }
+                for (Movie movie : list) {
+                    indexServiceImpl.insertMovie(movie);
+                }
+                total += list.size();
+                System.out.println("正在爬取中---共抓取:" + total + "条数据");
             } catch (Exception e) {
                 e.printStackTrace();
             }
---
+++ src/main/java/com/zjj/aisearch/crawler/Main1.java
@@ -0,0 +1,11 @@
+package com.zjj.aisearch.crawler;
+
+public class Main1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("ddddddddddd");
+        Thread.sleep(3000);
+        System.out.println("123132");
+    }
+
+}
