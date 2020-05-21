9d889e4a6a8d089944fc215864656eeee419a47d
QuietClickCode <2572417548@qq.com>
2020-05-21 14:07:54 +0800

    竟可能多和详细的获取用户信息,cookie设么的都用上,用户的每次操作,都要记录,时间,地点位置,设备,系统什么的,关联起来

--- src/main/java/com/zjj/aisearch/controller/IndexController.java
+++ src/main/java/com/zjj/aisearch/controller/IndexController.java
@@ -157,6 +157,7 @@ public class IndexController {
         return systenInfo;
 
     }
+
     /**
      * 获取用户真实IP地址，不使用request.getRemoteAddr();的原因是有可能用户使用了代理软件方式避免真实IP地址。
      * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值，究竟哪个才是真正的用户端的真实IP呢？
